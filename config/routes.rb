01ea54d0b42e368dd9e3825f8fcfc480cff7393a
Mikail Tavukcuoglu <mika.tavuk@gmail.com>
2020-08-14 12:44:41 +0200

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #  get 'pages/home'
+  resources :cocktails, only: [:index, :show, :create, :new] do 
+    resources :doses, only: [:new, :create]
+  end
+  
+  resources :doses, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+  resources :articles, except: :index
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# cocktails = Cocktail.create([{name: "mojito" }])
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+# Cocktail.create(name: "mojito")
+# Cocktail.create(name: "bloody mary")
+# Cocktail.create(name: "black velvet")
+
+# Dose.create (description: "6cl of lemon")
+# Dose.create (description: "1cl of vodka")
+# Dose.create (description: "3cl of beer")
